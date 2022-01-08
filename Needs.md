7cec0c653e92a1afba176a1ebe430c12c4cad9dd
Trey <59074512+Kiddxtrizz@users.noreply.github.com>
2022-01-07 22:41:35 -0500

    initial commits
    updated app.py

---
+++ Needs.md
@@ -0,0 +1,19 @@
+As a social equity participant, I have first-hand experience attempting to derive information from the available sources located on the CCC Open Data portal. Based on my research, here is a list of problems many other social equity participants and I have encountered.  
+
+1.	While the municipality zoning tracker is a valuable starting point for identifying which towns/cities cannabis businesses are allowed to operate in, there is currently no efficient method to determine the following (not limited to):
+    a)	Is the municipality accepting licenses? 
+    b)	How many licenses are available?
+    c)	What is the license cap?
+    d)	How many HCAs have been executed?
+    These are essential and vital questions that all cannabis businesses need answers to.
+2.	As cultivators continue to crop up, potential operators should be aware of the outstanding canopy available. 
+3.	The lack of transparency around the specific zoning boundaries for a municipality makes it challenging to determine the viability of any location for cannabis businesses. 
+
+    a.	Interpreting zoning overlays and boundaries:
+    
+    b.	Examples
+    1.	https://files.masscec.com/Quincy%20zoningmap-2014.pdf
+    
+    c.	Where are the acceptable zones located? 
+    
+Many other problems persist and derail the plans of many potential operators; these are just some of the prevalent issues social equity participants face.  
--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from helper_functions import get_data, choose_dataset, convert_to_csv
+from helper_functions import get_data, choose_dataset, convert_to_csv, load_data
 import streamlit as st
 import time
 import requests
 import re
+import pickle
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.io as pio
+
+api_token = 'pk.eyJ1Ijoia2lkZHRyaXp6IiwiYSI6ImNrbGk4dHp2cjRsZXAycG5yZTZ5cGFqNHIifQ.x5o6rqASgACRb4fuTSYkYg'
 
 st.set_page_config(
     page_title = "The Green Rush",
@@ -35,9 +41,29 @@ choice = st.sidebar.selectbox("Webpage Menu", menu)
 
 if choice == "Home":
     st.empty()
-    st.markdown("# Welcome to My Homepage" +" "+ "👋🏾 👊🏾 🧑🏽‍🚀  👨🏾‍💻 👨🏾‍🎨 ")
-    st.sidebar.header("My Name is what?!!?")
+    st.markdown("# Welcome to My Homepage" +" "+ "👋🏾 👊🏾")
+    st.balloons()
     
+    with st.container():
+        st.header("The Proposal")
+        with st.expander("Click to learn More"):
+            st.markdown("The purpose of this web application is to highlight the need for a tool to help streamline the location selection, permitting, and licensing process for (Social Equity, Economic Empowerment, Boston Equity) program participants.")
+            st.markdown("As a social equity participant alum, I have first-hand experience with the rigors of securing a location for a business. The process is cumbersome and expensive,\
+    from utilizing outdated PDFs of zoning maps (if they exist) to relying solely on the municipality's bylaws/ordinances or other obscure sources. Although these resources exist to help facilitate economic development, a predominant portion are dispersed amongst web pages, paper files, and overburdened town officials. Making the process time-consuming, inefficient, and confounding for SE/EE/BE participants and other entrepreneurs looking to enter the market." )
+            st.markdown("To resolve this problem, I have consolidated much of this information into a one-stop web and mobile platform that centralizes and captures relevant market and municipal information to help provide support to and underserved majority trying to entrer a burgeoning industry.")
+    
+    st.header("Problems to Address")
+    with st.expander("Click to learn More"):
+        string = open('Needs.md').read()
+        st.markdown(string)
+    
+    st.header("Project Overview")
+    with st.expander("Click to learn More"):
+        st.image('Picture3.png')
+    
+    st.header("Evaluation")
+    with st.expander("Click to learn More"):
+        st.markdown("The team will be implementing SCRUM methodology to track and evaluate success. The team will produce a minimum viable product to showcase to all stakeholders after each sprint. To ensure the progress is tracked – a burndown chart will be used to mark every successful sprint and maintain the velocity of each sprint.")
     
 elif choice == "Search":
     st.empty()
@@ -80,7 +106,8 @@ elif choice == "Search":
             with col2: 
                 ads = st.text_input("Advanced Search: Please select a column header first", value="")
 
-        if ads != "":    
+        if ads != "":
+            
             st.dataframe(data[data[col_selector].str.find(ads) != -1], 1450,600)
         else:
             st.dataframe(data,1450,600)
@@ -96,27 +123,107 @@ elif choice == "Search":
         )
         
     else:
-        with st.expander("Click to view filters"):
-            col1, col2, col3, col4 = st.columns(4)
-            
-            with col1: 
-                col_selector = st.selectbox("Select a column to view", list(results.columns))
-                if not col_selector:
-                    st.error("Please select at least one element")
+        st.sidebar.empty()
+        vsc = st.sidebar.radio('Select Chart Type', options=['Bar', 'Line'])
         
-        if len(data[col_selector].unique()) <= 20:
-            st.bar_chart(data[col_selector].value_counts() , 1450,600)
+        if vsc == 'Bar':
+            with st.expander("Click to view filters"):
+                col1, col2, col3, col4 = st.columns(4)
+
+                with col1: 
+                    x = st.selectbox("Select a column for x-axis", list(results.columns))
+
+                with col2:
+                    y = st.selectbox("Select a column for y-axis", list(results.columns))
+
+            with st.container():
+                fig = px.bar(data, x=x, y=list(range(len(data[y]))), title=f"{x} by {y}", 
+                               height = 800,
+                               width = 1450,)
+                fig.update_yaxes(type='category')
+                st.plotly_chart(fig, sharing='streamlit')
         else:
-            st.write('Way too many elements to display')
+            with st.expander("Click to view filters"):
+                col1, col2, col3 = st.columns(3)
+
+                with col1: 
+                    x = st.selectbox("Select a column for x-axis", list(results.columns))
+
+                with col2:
+                    y = st.selectbox("Select a column for y-axis", list(results.columns))
+
+            with st.container():
+                fig = px.line(data, x=x, y=y, title=f"{x} by {y}", 
+                               height = 800,
+                               width = 1450,)
+                st.plotly_chart(fig, sharing='streamlit')
+        
+#         if len(data[col_selector].unique()) <= 20:
+#             st.bar_chart(data[col_selector].value_counts() , 1450,600)
+#         else:
+#             st.write('Way too many elements to display')
             
 elif choice == 'Market Research':
-    st.empty()
-    st.header("Coming Soon")
+    #load in data
+    df = load_data()
+    
+    # #create pipeline
+    # pipe = Pipeline([('Standardize', StandardScaler()),
+    #                 ('PCA', PCA()),
+    #                 ('Kmeans', KMeans())])
+    
+    #load models in
+    scaler = pickle.load(open('scaler.pickle', 'rb'))
+    pca = pickle.load(open('pca.pickle', 'rb'))
+    kmeans_pca = pickle.load(open('kmeans_pca.pickle', 'rb'))
+    
+    # get features to transform 
+    features = df[['dest_long', 'dest_lat', 'zip_code','population_denisty', 'median_household_income', 'sex','distance','DOB_year','age','Settlement_size']]
+    
+    #standardize
+    df_seg_scaled = scaler.transform(features)
+    
+    #Reduce dimesionality 
+    df_seg_pca = pca.transform(df_seg_scaled)
+    
+    #Cluster data points
+    df_kmeans_pca = kmeans_pca.predict(df_seg_pca)
+    
+    #combine it altogether 
+    df_segm = df.copy()
+    df_segm['segment'] = df_kmeans_pca 
+    
+    #
+    dff = df_segm[['dest_long', 'dest_lat', 'zip_code','population_denisty', 'median_household_income', 'sex','distance','DOB_year','age','Settlement_size', 'segment']]
+    dff = dff.rename(columns={'dest_long': 'lon','dest_lat': 'lat'})
+    df_segm_analysis = dff.groupby(['segment']).mean()
+    df_segm_analysis['N obs'] = df_segm[['segment', 'age']].groupby(['segment']).count()
+    df_segm_analysis['Prop Obs'] = df_segm_analysis['N obs']/df_segm_analysis['N obs'].sum()
+    df_segm_analysis = df_segm_analysis.rename({0:'Working Professional', 1:'Standard', 2:'Career Focused', 3:'Well-Off'})
+    
+    with st.container():
+        col1,col2 = st.columns(2)
+        
+        # st.map(dff[['lon', 'lat']])
+        with col1:
+            fig = px.scatter_mapbox(dff, lat='lat', lon='lon', color='segment', 
+                                   height = 800,
+                                   width = 600,
+                                   title='Segmented Consumer Map',
+                                   )
 
+            fig.update_layout(
+                mapbox_style="mapbox://styles/kiddtrizz/cky3fdrla2fun15nzclck4mdu",
+                mapbox_accesstoken="pk.eyJ1Ijoia2lkZHRyaXp6IiwiYSI6ImNreTNoeHNiZTAyN3czMm8wYmthZXh1Z3oifQ.5_xkXfXF1EXDnORZIn40Xw"
+            )
+
+            st.plotly_chart(fig, sharing='streamlit')
+        with col2:
+            st.dataframe(df_segm_analysis, height=1450, width=800)
 else:
     st.empty()
-    st.header("About")
-
+    st.header("About Me")
+    st.markdown("My name is Trey W. I'm a Boston (Roxbury) Native who has enjoyed over 20 years in the beautiful city of Boston, and I couldn't be luckier. To top that, I have had the pleasure of attending Northeastern University and obtaining the title of Double Husky after completing both my undergraduate and graduate degrees from the prestigious university. During my graduate studies, I saw a chance to apply my unique perspective, critical thinking skills, and entrepreneurial mindset to various projects and tasks. As a result, I have decided to create a place to showcase my thoughts, ideas, and projects. I hope you enjoy it! Feel free to contact me if you have any questions.")
 # except Exception as e:
 #     st.error('What happened')
 # except requests.exceptions.HTTPError as errh:
